5ee66b0fab381372ad4b038e447d7c7089500301
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2015-10-06 14:27:53 +0900

    ファイル削除及びファイル名変更

--- sqlplus.sh
+++ sqlplus.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
-LD_LIBRARY_PATH=/home/yamauchi/oracle/instantclient_12_1
-PATH=/home/yamauchi/oracle/instantclient_12_1:$PATH
+#!/bin/sh
+
+# 各自の環境に合わせて記述
+LD_LIBRARY_PATH=/home/yamauchi/instantclient_12_1
+PATH=/home/yamauchi/instantclient_12_1:$PATH
+
+# 以降は共通
 NLS_LANG=JAPANESE_JAPAN.AL32UTF8
 export LD_LIBRARY_PATH PATH NLS_LANG
-rlwrap sqlplus hr/hr@172.16.40.4:1521/db11
+export no_proxy=localhost,172.16.40.4
+echo -n "ユーザ名："
+read user
+stty -echo
+echo -n "パスワード："
+read pass
+stty echo
+rlwrap sqlplus $user/$pass@172.16.40.4:1521/db11
+#rlwrap sqlplus $user/$pass@172.16.40.4:1521/db11 as sysdba
