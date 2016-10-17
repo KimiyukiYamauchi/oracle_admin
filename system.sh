213ef057e0c3c23f22b2bf5cd7c37f27f4f2af71
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-10-17 16:55:59 +0900

    サーバのIPアドレスの変更等の修正

--- system.sh
+++ system.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # 各自の環境に合わせて記述
-LD_LIBRARY_PATH=/home/yamauchi/instantclient_12_1
-PATH=/home/yamauchi/instantclient_12_1:$PATH
+LD_LIBRARY_PATH=/home/application/instantclient_12_1
+PATH=/home/application/instantclient_12_1:$PATH
 
 # 以降は共通
 NLS_LANG=JAPANESE_JAPAN.AL32UTF8
 export LD_LIBRARY_PATH PATH NLS_LANG
-export no_proxy=localhost,172.16.40.4
+export no_proxy=localhost,192.168.30.4
 stty -echo
 echo -n "パスワード："
 read pass
 stty echo
-rlwrap sqlplus system/$pass@172.16.40.4:1521/db11
+rlwrap sqlplus system/$pass@192.168.30.4:1521/db11
